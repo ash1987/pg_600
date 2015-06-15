363c068dcc4f71237dd4d8f405328a3dfda5e00d
liujl <562626519@qq.com>
2015-06-15 23:59:01 +0800

    add some data

--- src/com/company/pg/Define.java
+++ src/com/company/pg/Define.java
@@ -10,8 +10,60 @@ public class Define {
 	//机制云注册产品key
 	public static final String ProductKey = "a6597c37c5944dadb1ad8f9ea5c60de5";
 	
-	public static final String entity = "11111";
+	/** 布防 标识名：bufan_Off . */
+	public static final String KEY_BUFANG = "attr0";
+
+	/** 撤防 标识名：chefang_off */
+	public static final String KEY_CHEFANG = "attr1";
+
+	/** 在家布防 标识名：zbfan_off*/
+	public static final String KEY_ZAIJIABUFANG = "attr2";
+
+	/** 紧急求救 标识名：jinjbj_off */
+	public static final String KEY_JINJIQIUJIU = "attr3";
+
+	/** 声光报警 标识名：sgbj */
+	public static final String KEY_SHENGGUANGBAOJIN = "attr4";
+
+	/** 警号开关 标识名：jinghao_onoff */
+	public static final String KEY_JINGHAOKAIGUANG = "attr5";
+
+	/** 灯光开关 标识名：LED_onoff */
+	public static final String KEY_DENGGUANGKAIGUANG = "attr6";
+
+	/** 信息查询 标识名：xxsc 20char*/
+	public static final String KEY_XINXICHAXUN = "attr7";
+
+	/** 遥控1学习 标识名：yaokon1xs   num 0 - 254*/
+	public static final String KEY_YAOKONGQIXUEXI = "attr8";
+
+	/** 探测器1学习 标识名：chuceqi1   num 0 - 254*/
+	public static final String KEY_TANCEQIXUEXI = "attr9";
 	
-	public static final String KEY_BUFANG ="BUFANG";
+	
+	/** 警号学习 标识名：jing 0.触发, 1.清涂, 2.重命名*/
+	public static final String KEY_JINGHAOXUEXI = "attr10";
+	
+	/** 遥控删除 标识名：pjst   num 0 - 5*/
+	public static final String KEY_YAOKONGSHANCHU = "attr11";
+	
+	/** 进入延迟时间设置 标识名：Culrsj   num 0 - 254*/
+	public static final String KEY_JINRUYANCHI = "attr12";
+	
+	/** 警号蜂鸣时间设置 标识名：jhfm   num 0 - 254*/
+	public static final String KEY_JINGHAOFENGMING = "attr13";
+	
+	/** 探测器删除 标识名：tcqs   num 0 - 50*/
+	public static final String KEY_TANCEQISHANCHU = "attr14";
+	
+	/**显示名称：警号删除 标识名：jinghaos*/
+	public static final String KEY_JINGHAOSHANCHU = "attr15";
+	
+	/** 主机语言设置 标识名：xiugaiyl  0.中文, 1.英文, 2.法文, 3.德文*/
+	public static final String KEY_LANGUAGE = "attr16";
+	
+	/** 实体字段名，代表对应的项目. */
+	public static final String KEY_ACTION = "PN-600";
+
 
 }
--- src/com/company/pg/ui/IntelligentConfigureActivity.java
+++ src/com/company/pg/ui/IntelligentConfigureActivity.java
@@ -1,11 +1,21 @@
 package com.company.pg.ui;
 
+import android.app.ProgressDialog;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.company.pg.R;
 import com.company.pg.base.BaseActivity;
+import com.xtremeprog.xpgconnect.XPGWifiDevice;
 
 /*
  * 智能配置
@@ -14,6 +24,9 @@ public class IntelligentConfigureActivity extends BaseActivity implements OnClic
 	private TextView page_title;
 	private Button cancleBt;
 	private Button okBt;
+	private TextView wifiname;
+	private EditText wifipwd;
+	private Button bangdevice;
 
 	@Override
 	public void onClick(View v) {
@@ -28,13 +41,71 @@ public class IntelligentConfigureActivity extends BaseActivity implements OnClic
 		case R.id.okBt:
 			this.finish();
 			break;
+		case R.id.bangdevice:
+	
+				dialog.show();
+				String password = wifipwd.getText().toString().trim();
+				// 发送airlink广播，把需要连接的wifi的ssid和password发给模块。
+				mCenter.cSetAirLink(connnectedWifiName, password);
+				
+			break;
 		default:
 			break;
 		}
 	}
 
+
+	/** 配置成功. */
+	protected static final int SUCCESS = 0;
+
+	/** 配置失败. */
+	protected static final int FAIL = 1;
+
+	/** 配置超时. */
+	protected static final int TIEMOUT = 2;
+	/** The dialog. */
+	private ProgressDialog dialog;
+
+	/** The handler. */
+	private Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case FAIL:
+				dialog.cancel();
+				Toast.makeText(IntelligentConfigureActivity.this, "配置失败", Toast.LENGTH_SHORT)
+						.show();
+				break;
+			case SUCCESS:
+				dialog.cancel();
+				Toast.makeText(IntelligentConfigureActivity.this, "配置成功", Toast.LENGTH_SHORT)
+						.show();
+				finish();
+				break;
+
+			case TIEMOUT:
+				dialog.cancel();
+				Toast.makeText(IntelligentConfigureActivity.this, "配置超时", Toast.LENGTH_SHORT)
+						.show();
+				break;
+			}
+
+		}
+	};
+
+	
+    String connnectedWifiName;
+    
 	@Override
 	protected void initData() {
+		WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);  
+		WifiInfo wifiInfo = wifiManager.getConnectionInfo();  
+		Log.d("wifiInfo", wifiInfo.toString());  
+		if (wifiInfo!=null) {
+			connnectedWifiName= wifiInfo.getSSID();
+			Log.d("SSID",wifiInfo.getSSID());  
+		}else
+			connnectedWifiName="";
+		
 
 	}
 
@@ -45,13 +116,40 @@ public class IntelligentConfigureActivity extends BaseActivity implements OnClic
 		page_title = (TextView) findViewById(R.id.page_title);
 		cancleBt = (Button) findViewById(R.id.cancleBt);
 		okBt = (Button) findViewById(R.id.okBt);
+		wifiname = (TextView) findViewById(R.id.wifi_name);
+		wifipwd = (EditText)findViewById(R.id.wifipwd);
+		bangdevice = (Button) findViewById(R.id.bangdevice);
 	}
 
 	@Override
 	protected void setAttribute() {
 		cancleBt.setOnClickListener(this);
 		okBt.setOnClickListener(this);
-
+		bangdevice.setOnClickListener(this);
+		wifiname.setText(connnectedWifiName);
 		page_title.setText(R.string.smart_config_settings);
+		dialog = new ProgressDialog(this);
+		dialog.setMessage("配置中，请稍候...");
+	}
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		if (dialog!=null&&dialog.isShowing()) {
+			dialog.dismiss();
+			dialog=null;
+		}
+		super.onDestroy();
+	}
+	
+	/*
+	 * 配置成功回调
+	 * 
+	 * @see
+	 * com.xpg.gokit.activity.BaseActivity#didSetDeviceWifi(int error, XPGWifiDevice device)
+	 */
+	protected void didSetDeviceWifi(int error, XPGWifiDevice device) {
+		// 通过airlink配置模块成功连上路由器后，回调该函数。
+		Log.i("air link device", "" + device.getMacAddress() + " ");
+		handler.sendEmptyMessage(SUCCESS);
 	}
 }
