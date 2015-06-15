15da2ae32d08223dc1b71aca61b321cb20224a1a
liujl <5626262519@qq.com>
2015-06-15 15:46:31 +0800

    test

--- src/com/company/pg/Define.java
+++ src/com/company/pg/Define.java
@@ -9,5 +9,9 @@ public class Define {
 	public static final String APPID = "673d8dda9a6143848bdca0c1294667ed";
 	//机制云注册产品key
 	public static final String ProductKey = "a6597c37c5944dadb1ad8f9ea5c60de5";
+	
+	public static final String entity = "11111";
+	
+	public static final String KEY_BUFANG ="BUFANG";
 
 }
